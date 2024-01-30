090fdda8e3391b8cc728591923de5428d6240db2
mickdec <mickaeldecelle@hotmail.fr>
2024-01-30 18:26:54 +0100

    X

--- test.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.settimeout(1)
-s.connect(("10.129.46.139", "80"))
-data = s.send(b"GET / HTTP/1.1\r\nHost:www.example.com\r\n\r\n")
-data = s.recv(1024)
-print(data)
